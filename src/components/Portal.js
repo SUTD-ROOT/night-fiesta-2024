29eda7c233c97345dd5baaa37b1795a82d8a8e50
Rachel <80181684+rappleit@users.noreply.github.com>
2024-02-23 22:05:55 +0800

    changed leaderboard thumbnail

--- src/components/Portal.js
+++ src/components/Portal.js
@@ -5,6 +5,7 @@ import layoutThumbnail from "../assets/lvl1.png"
 import quizThumbnail from "../assets/quiz-thumbnail.jpg"
 import stageThumbnail from "../assets/stage-thumbnail.jpg"
 import merchThumbnail from "../assets/shirt-banner.png"
+import leaderboardThumbnail from "../assets/leaderboard_thumbnail.png"
 
 const Portal = () => {
     return (
@@ -19,25 +20,27 @@ const Portal = () => {
                     </Link>
                 </div>
                 <Link to="/schedule" className={styles.link}>
-                <div className={styles.card}>
-                    <img alt="" src={stageThumbnail} />
-                    <h2>STAGE SCHEDULE</h2>
-                </div>
+                    <div className={styles.card}>
+                        <img alt="" src={stageThumbnail} />
+                        <h2>STAGE SCHEDULE</h2>
+                    </div>
                 </Link>
-                {/*<div className={styles.card}>
-                    <img alt="" src={placeholder} />
-                    <h2>LEADERBOARD</h2>
-    </div>*/}
+                <div className={styles.card}>
+                    <a href="/leaderboard">
+                        <img alt="" src={leaderboardThumbnail} />
+                        <h2>LEADERBOARD</h2>
+                    </a>
+                </div>
                 <div className={styles.card}>
                     <a href="/quiz">
-                    <img alt="" src={quizThumbnail} />
-                    <h2>CHARACTER QUIZ</h2>
+                        <img alt="" src={quizThumbnail} />
+                        <h2>CHARACTER QUIZ</h2>
                     </a>
                 </div>
                 <div className={styles.card}>
                     <a href="/merch">
-                    <img alt="" src={merchThumbnail} />
-                    <h2>Merchandise</h2>
+                        <img alt="" src={merchThumbnail} />
+                        <h2>Merchandise</h2>
                     </a>
                 </div>
                 {/*<Link to="/team" className={styles.link}>
